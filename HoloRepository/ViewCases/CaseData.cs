ebc04577bf22a1b571053109d899171efc02841f
peichipeng <peichi426@gmail.com>
2024-07-23 22:19:58 +0100

    Refined view cases page

--- HoloRepository/ViewCases/CaseData.cs
+++ HoloRepository/ViewCases/CaseData.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HoloRepository
+namespace HoloRepository.ViewCases
 {
     public class CaseData
     {
         public int DonorId { get; set; }
         public DateTime DOD { get; set; }
         public int Age { get; set; }
-        public string causeOfDeath { get; set; }
-        public string organs { get; set; }
+        public string CauseOfDeath { get; set; }
+        public string Organs { get; set; }
     }
 }
---
+++ HoloRepository/ViewCases/DataRetrieval.cs
@@ -0,0 +1,27 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoloRepository.ViewCases
+{
+    public class DataRetrieval
+    {
+        private static string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=123456;Database=HoloRepository";
+
+        public static NpgsqlDataSource CreateDataSource()
+        {
+            return NpgsqlDataSource.Create(connectionString);
+        }
+
+        public static async Task<NpgsqlDataReader> ExecuteQuery (string query, NpgsqlDataSource dataSource)
+        {
+            await using var command = dataSource.CreateCommand(query);
+
+            return await command.ExecuteReaderAsync();
+        }
+    }
+}
